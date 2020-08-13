ab2b766dd73d410fe163dab9f41ed11a250672ec
Kamogelo Matlhakola <kamogelo.matlhakola@ccssa.com>
2020-08-13 07:10:24 +0200

    Add project files.

---
+++ GameOfLife/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace GameOfLife
+{
+    
+
+    internal class Program
+    {
+
+        // Constants for the game rules.
+        private const int Heigth = 10;
+        private const int Width = 50;
+        private const uint MaxRuns = 100;
+
+        private static void Main(string[] args)
+        {
+            int runs = 0;
+            var sim = new GameOfLifeService(Heigth, Width);
+
+            while (runs++ < MaxRuns)
+            {
+                sim.DrawAndGrow();
+
+                // Give the user a chance to view the game in a more reasonable speed.
+                System.Threading.Thread.Sleep(100);
+            }
+            Console.ReadKey();
+        }
+    }
+}
